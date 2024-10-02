0aab0448b02e78ad9425b46e26cddf45a0b8abaa
overdd <antonsiarko@gmail.com>
2024-10-02 22:02:43 +0200

    urls are listed on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import { PrismaClient } from '@prisma/client';
 
-export default function Home() {
+const prismaClient = new PrismaClient();
+
+export default async function Home() {
+
+  const values = await prismaClient.urls.findMany();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -21,6 +27,9 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          {values.map((value, index) => (
+           <li key={index}>{value.shortUrl} {"=>"} {value.longUrl}</li>
+         ))}
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
